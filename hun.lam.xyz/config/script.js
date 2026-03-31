6a0da836abc2b30c8aa73a95a0b99826581fae58
baukobojan <bauko.bojan1@gmail.com>
2026-03-31 18:56:20 +0200

    jegyertekesites-page

--- hun.lam.xyz/config/script.js
+++ hun.lam.xyz/config/script.js
@@ -5,3 +5,33 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('change');
     dropdown.classList.toggle('open');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    showSlides(slideIndex);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}});
